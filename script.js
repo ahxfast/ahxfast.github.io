aac28571fc15a01d0dc6628bbec8cb7a1a3240bb
GuidoDirr <guido.dirr@ocean.ibm.com>
2023-12-01 17:53:34 +0100

    beer

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ document.addEventListener('DOMContentLoaded', function () {
         container.appendChild(bubble);
     }
 });
+
+
+function updateCountdown() {
+    var now = new Date();
+    var eventDate = new Date(2023, 11, 15, 18, 0, 0); // December is 11 in JavaScript (0-indexed)
+    var currentTime = now.getTime();
+    var eventTime = eventDate.getTime();
+    var remTime = eventTime - currentTime;
+
+    var s = Math.floor(remTime / 1000);
+    var m = Math.floor(s / 60);
+    var h = Math.floor(m / 60);
+    var d = Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").textContent = d;
+    document.getElementById("hours").textContent = h;
+    document.getElementById("minutes").textContent = m;
+    document.getElementById("seconds").textContent = s;
+
+    setTimeout(updateCountdown, 1000);
+}
+
+updateCountdown();

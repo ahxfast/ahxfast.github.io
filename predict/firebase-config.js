7f8a7234366696d2cd9379595ba0de147ef26ded
ahxfast <ahxfast@gmail.com>
2024-12-13 08:43:50 +0100

    predict

---
+++ predict/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyCfWKsBd68Hn6c9NI2yj4pjeWphMWl5mBk",
+      authDomain: "cryptowatcher-8dae1.firebaseapp.com",
+      databaseURL: "https://cryptowatcher-8dae1-default-rtdb.firebaseio.com",
+      projectId: "cryptowatcher-8dae1",
+      storageBucket: "cryptowatcher-8dae1.appspot.com",
+      messagingSenderId: "303293127609",
+      appId: "1:303293127609:web:b8c7578708975ba44b3468",
+      measurementId: "G-2B1P2PD2L1"
+  };
